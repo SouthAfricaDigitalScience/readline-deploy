2d2ec6c4f3d5379e56413697eaa91ee773dc37ba
Bruce Becker <brucellino@gmail.com>
2017-05-05 12:33:27 +0200

    Preparation for release

--- check-build.sh
+++ check-build.sh
@@ -1,6 +1,20 @@
 #!/bin/bash -e
+# Copyright 2016 C.S.I.R. Meraka Institute
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 . /etc/profile.d/modules.sh
-module load ci
+module add ci
 module add ncurses
 cd ${WORKSPACE}/${NAME}-${VERSION}/build-${BUILD_NUMBER}
 echo "installing ${NAME}"
@@ -30,8 +44,8 @@ prepend-path PATH                   $::env(READLINE_DIR)/bin
 MODULE_FILE
 ) > modules/${VERSION}
 
-mkdir -p ${LIBRARIES_MODULES}/${NAME}
-cp modules/${VERSION} ${LIBRARIES_MODULES}/${NAME}
+mkdir -p ${LIBRARIES}/${NAME}
+cp modules/${VERSION} ${LIBRARIES}/${NAME}
 
 module unload ci
 echo "re-adding CI module"
